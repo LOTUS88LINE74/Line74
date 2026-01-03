952963823e84f6d140b32e42fdb54f2756a72429
LOTUS88LINE74 <id1.freefiremax@gmail.com>
2026-01-04 01:11:30 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ function filterGame(type) {
   const cards = document.querySelectorAll('.card');
 
   cards.forEach(card => {
-    if (type === 'all' || card.classList.contains(type)) {
+    if (type === 'all') {
+      card.style.display = 'block';
+    } else if (card.classList.contains(type)) {
       card.style.display = 'block';
     } else {
       card.style.display = 'none';
